91538a332a5f0e28e4e3281a8c446ef5d283e3df
Ilaaksh Mishra <ilaakshmishra@Ilaakshs-MacBook-Air.local>
2023-04-12 18:00:34 -0400

    Obj Get class

--- Assignment_5/startUpfile/src/Env.java
+++ Assignment_5/startUpfile/src/Env.java
@@ -36,7 +36,13 @@ public class Env
         map.put("<=", "LE");
         map.put("=", "EQ");
         map.put(";", "SEMI");
+        map.put(",", "COMMA");
         map.put("int", "INT");
+        map.put("and", "AND");
+        map.put("or", "OR");
+        map.put("not", "NOT");
+        map.put("true", "BOOL_LIT");
+        map.put("false", "BOOL_LIT");
         map.put("if"," IF");
         map.put("else", "ELSE");
         map.put("while"," WHILE");
@@ -47,9 +53,12 @@ public class Env
         // For the real implementation, I recommend to return a class object
         //   since the identifier's type can be variable or function
         //   whose detailed attributes will be different
-        if(name.equals("a") == true) return "int";
-        if(name.equals("b") == true) return "bool";
-        if(name.equals("testfunc") == true) return "func()->int";
+        if(name.equals("func") == true) return "func";
+        if(name.equals("call") == true) return "call";
+        if(name.equals("return") == true) return "return";
+        if(name.equals("var") == true) return "var";
+        if(name.equals("if") == true) return "if";
+        if(name.equals("else") == true) return "else";
         return null;
     }
 }
